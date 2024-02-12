686bfdf801768f6eb74ed1c2fb882f3bd5d8922e
Marlon Stiff <m.londonot@uniandes.edu.co>
2024-02-12 00:03:16 -0500

    cambios taller 2

---
+++ nTaller2EstructurasEsqueleto/nTaller2EstructurasEsqueleto/src/uniandes/dpoo/estructuras/logica/SandboxListas.java
@@ -0,0 +1,342 @@
+package uniandes.dpoo.estructuras.logica;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Esta clase tiene un conjunto de métodos para practicar operaciones sobre listas de enteros y de cadenas.
+ *
+ * Todos los métodos deben operar sobre los atributos listaEnteros y listaCadenas.
+ * 
+ * No pueden agregarse nuevos atributos.
+ * 
+ * Implemente los métodos usando operaciones sobre listas (ie., no haga cosas como construir arreglos para evitar la manipulación de listas).
+ * 
+ * Intente usar varias formas de recorrer las listas (while, for, for each, iteradores ... )
+ */
+public class SandboxListas
+{
+    /**
+     * Una lista de enteros para realizar varias de las siguientes operaciones.
+     */
+    private List<Integer> listaEnteros;
+
+    /**
+     * Una lista de cadenas para realizar varias de las siguientes operaciones
+     */
+    private List<String> listaCadenas;
+
+    /**
+     * Crea una nueva instancia de la clase con las dos listas inicializadas pero vacías
+     */
+    public SandboxListas( )
+    {
+        listaEnteros = new ArrayList<Integer>( );
+        listaCadenas = new LinkedList<String>( );
+    }
+
+    /**
+     * Retorna una copia de la lista de enteros, es decir una nueva lista del mismo tamaño que contiene copias de los valores de la lista original
+     * @return Una copia de la lista de enteros
+     */
+    public List<Integer> getCopiaEnteros( )
+    {
+    	return new ArrayList<>(listaEnteros);
+    }
+
+    /**
+     * Retorna una copia de la lista de cadenas, es decir una nueva lista del mismo tamaño que contiene copias de los valores de la lista original
+     * @return Una copia de la lista de cadenas
+     */
+    public List<String> getCopiaCadenas( )
+    {
+        return new ArrayList<>(listaCadenas);
+    }
+
+    /**
+     * Retorna un arreglo con los valores de la lista de enteros, es decir un arreglo del mismo tamaño que contiene copias de los valores de la lista
+     * @return Una arreglo de enteros
+     */
+    public int[] getEnterosComoArreglo( )
+    {
+    	int[] arreglo=new int[listaEnteros.size()];
+    	for (int i = 0; i < listaEnteros.size(); i++) {
+			arreglo[i]=listaEnteros.get(i);
+		}
+        return arreglo;
+    }
+
+    /**
+     * Retorna la cantidad de valores en la lista de enteros
+     * @return
+     */
+    public int getCantidadEnteros( )
+    {
+        return listaEnteros.size();
+    }
+
+    /**
+     * Retorna la cantidad de valores en la lista de cadenas
+     * @return
+     */
+    public int getCantidadCadenas( )
+    {
+        return listaCadenas.size();
+    }
+
+    /**
+     * Agrega un nuevo valor al final de la lista de enteros. Es decir que este método siempre debería aumentar en 1 el tamaño de la lista.
+     * 
+     * @param entero El valor que se va a agregar.
+     */
+    public void agregarEntero( int entero )
+    {
+    	listaEnteros.add(entero);
+    }
+
+    /**
+     * Agrega un nuevo valor al final de la lista de cadenas. Es decir que este método siempre debería aumentar en 1 la capacidad de la lista.
+     * 
+     * @param cadena La cadena que se va a agregar.
+     */
+    public void agregarCadena( String cadena )
+    {
+    	listaCadenas.add(cadena);
+    }
+
+    /**
+     * Elimina todas las apariciones de un determinado valor dentro de la lista de enteros
+     * @param valor El valor que se va eliminar
+     */
+    public void eliminarEntero( int valor )
+    {
+    	for (int j = 0; j < listaEnteros.size(); j++) {
+			if (listaEnteros.get(j)==valor) {
+				listaEnteros.remove(j);
+			}
+			
+		}
+    }
+
+    /**
+     * Elimina todas las apariciones de un determinado valor dentro de la lista de cadenas
+     * @param cadena La cadena que se va eliminar
+     */
+    public void eliminarCadena( String cadena )
+    {
+    	for (int j = 0; j < listaCadenas.size(); j++) {
+			if (listaCadenas.get(j)==cadena) {
+				listaCadenas.remove(j);
+			}
+			
+		}
+    }
+
+    /**
+     * Inserta un nuevo entero en la lista de enteros
+     * 
+     * @param entero El nuevo valor que debe agregarse
+     * @param posicion La posición donde debe quedar el nuevo valor en la lista aumentada. Si la posición es menor a 0, se inserta el valor en la primera posición. Si la
+     *        posición es mayor que el tamaño de la lista, se inserta el valor en la última posición.
+     */
+    public void insertarEntero( int entero, int posicion )
+    {
+    	if (posicion < 0) {
+            listaEnteros.add(0, entero); 
+        } else if (posicion > listaEnteros.size()) {
+            listaEnteros.add(entero);
+        } else {
+            listaEnteros.add(posicion, entero);
+        }
+    }
+
+    /**
+     * Elimina un valor de la lista de enteros dada su posición.
+     * @param posicion La posición donde está el elemento que debe ser eliminado. Si el parámetro posicion no corresponde a ninguna posición de la lista de enteros, el método
+     *        no debe hacer nada.
+     */
+    public void eliminarEnteroPorPosicion( int posicion )
+    {
+    	if (posicion >= 0 && posicion < listaEnteros.size()) {
+            listaEnteros.remove(posicion);
+        }
+    }
+
+    /**
+     * Reinicia la lista de enteros con los valores contenidos en el parámetro 'valores', pero truncados.
+     * 
+     * Es decir que si el valor fuera 3.67, en la nueva lista debería quedar el entero 3.
+     * @param valores Un arreglo de valores decimales.
+     */
+    public void reiniciarArregloEnteros( double[] valores )
+    {
+    	for (double valor : valores) {
+            int enteroTruncado = (int) Math.floor(valor);
+            listaEnteros.add(enteroTruncado);
+    	}
+    }
+
+    /**
+     * Reinicia la lista de cadenas con las representaciones como Strings de los objetos contenidos en la lista del parámetro 'objetos'.
+     * 
+     * Use el método toString para convertir los objetos a cadenas.
+     * @param valores Una lista de objetos
+     */
+    public void reiniciarArregloCadenas( List<Object> objetos )
+    {
+    	List<String> listaCadenas1= new ArrayList<String>( );
+    	for (Object objeto : objetos) {
+            listaCadenas1.add(objeto.toString());
+        }
+    	
+    	listaCadenas=listaCadenas1;
+    }
+
+    /**
+     * Modifica la lista de enteros para que todos los valores sean positivos.
+     * 
+     * Es decir que si en una posición había un valor negativo, después de ejecutar el método debe quedar el mismo valor muliplicado por -1.
+     */
+    public void volverPositivos( )
+    {
+    	for (int i = 0; i < listaEnteros.size(); i++) {
+            int valor = listaEnteros.get(i);
+            if (valor < 0) {
+                listaEnteros.set(i, valor * -1);
+            }
+    	}
+    }
+
+    /**
+     * Modifica la lista de enteros para que todos los valores queden organizados de MAYOR a MENOR.
+     */
+    public void organizarEnteros( )
+    {
+    	Collections.sort(listaEnteros, Collections.reverseOrder());
+    }
+
+    /**
+     * Modifica la lista de cadenas para que todos los valores queden organizados lexicográficamente.
+     */
+    public void organizarCadenas( )
+    {
+    	Collections.sort(listaCadenas);
+    }
+
+    /**
+     * Cuenta cuántas veces aparece el valor recibido por parámetro en la lista de enteros
+     * @param valor El valor buscado
+     * @return La cantidad de veces que aparece el valor
+     */
+    public int contarApariciones( int valor )
+    {
+    	int contador = 0;
+
+        for (int elemento : listaEnteros) {
+            if (elemento == valor) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    /**
+     * Cuenta cuántas veces aparece la cadena recibida por parámetro en la lista de cadenas.
+     * 
+     * La búsqueda no debe diferenciar entre mayúsculas y minúsculas.
+     * @param cadena La cadena buscada
+     * @return La cantidad de veces que aparece la cadena
+     */
+    public int contarApariciones( String cadena )
+    {
+    	int contador = 0;
+
+        
+        for (String elemento : listaCadenas) {
+            if (elemento.equalsIgnoreCase(cadena)) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    /**
+     * Cuenta cuántos valores dentro de la lista de enteros están repetidos.
+     * @return La cantidad de enteos diferentes que aparecen más de una vez
+     */
+    public int contarEnterosRepetidos( )
+    {
+    	int contadorRepetidos = 0;
+    	
+        for (int i = 0; i < listaEnteros.size(); i++) {
+            int elementoActual = listaEnteros.get(i);
+            boolean repetido = false;
+
+            for (int j = 0; j < listaEnteros.size(); j++) {
+                if (i != j && elementoActual == listaEnteros.get(j)) {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (repetido) {
+                contadorRepetidos++;
+            }
+        }
+
+        return contadorRepetidos;
+    }
+    
+
+    /**
+     * Compara la lista de enteros con un arreglo de enteros y verifica si contienen los mismos elementos exactamente en el mismo orden.
+     * @param otroArreglo El arreglo de enteros con el que se debe comparar
+     * @return True si los elementos son los mismos y en el mismo orden y false de lo contrario
+     */
+    public boolean compararArregloEnteros( int[] otroArreglo )
+    {
+    	int i=0;
+    	boolean igual=true;
+    	if (otroArreglo.length==listaEnteros.size()) {
+    		while (igual) {
+				if (listaEnteros.get(i)!=otroArreglo[i]) {
+					igual=false;
+				}
+			} 
+    	}else {
+    		igual=false;
+    	}
+    	
+    	
+        return igual;
+    }
+
+    /**
+     * Cambia los elementos de la lista de enteros por una nueva serie de valores generada de forma aleatoria.
+     * 
+     * Para generar los valores se debe partir de una distribución uniforme usando Math.random().
+     * 
+     * Los números en la lista deben quedar entre el valor mínimo y el máximo.
+     * @param cantidad La cantidad de elementos que debe haber en la lista
+     * @param minimo El valor mínimo para los números generados
+     * @param maximo El valor máximo para los números generados
+     */
+    public void generarEnteros( int cantidad, int minimo, int maximo )
+    {
+    	
+        listaEnteros.clear();
+
+        
+        for (int i = 0; i < cantidad; i++) {
+            int nuevoValor = (int) (Math.random() * (maximo - minimo + 1) + minimo);
+            listaEnteros.add(nuevoValor);
+        }
+    }
+
+    
+
+}
---
+++ nTaller2EstructurasEsqueleto/nTaller2EstructurasEsqueleto/src/uniandes/dpoo/estructuras/logica/SandboxMapas.java
@@ -0,0 +1,200 @@
+package uniandes.dpoo.estructuras.logica;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Esta clase tiene un conjunto de métodos para practicar operaciones sobre mapas.
+ *
+ * Todos los métodos deben operar sobre el atributo mapaCadenas que se declara como un Map.
+ * 
+ * En este mapa, las llaves serán cadenas y los valores serán también cadenas. La relación entre los dos será que cada llave será igual a la cadena del valor, pero invertida.
+ * 
+ * El objetivo de usar el tipo Map es que sólo puedan usarse métodos de esa interfaz y no métodos adicionales provistos por la implementación concreta (HashMap).
+ * 
+ * No pueden agregarse nuevos atributos.
+ */
+public class SandboxMapas
+{
+    /**
+     * Un mapa de cadenas para realizar varias de las siguientes operaciones.
+     * 
+     * Las llaves del mapa son cadenas, así como los valores.
+     * 
+     * Las llaves corresponden a invertir la cadena que aparece asociada a cada llave.
+     */
+    private Map<String, String> mapaCadenas;
+
+    /**
+     * Crea una nueva instancia de la clase con las dos listas inicializadas pero vacías
+     */
+    public SandboxMapas( )
+    {
+        mapaCadenas = new HashMap<String, String>( );
+    }
+
+    /**
+     * Retorna una lista con las cadenas del mapa (los valores) ordenadas lexicográficamente
+     * @return Una lista ordenada con las cadenas que conforman los valores del mapa
+     */
+    public List<String> getValoresComoLista( )
+    {
+    	List<String> listaValores = new ArrayList<>(mapaCadenas.values());
+
+        
+        Collections.sort(listaValores);
+        
+        return listaValores;
+    }
+
+    /**
+     * Retorna una lista con las llaves del mapa ordenadas lexicográficamente de mayor a menor
+     * @return Una lista ordenada con las cadenas que conforman las llaves del mapa
+     */
+    public List<String> getLlavesComoListaInvertida( )
+    {
+    	List<String> listaLlaves = new ArrayList<>(mapaCadenas.keySet());
+
+        
+        Collections.sort(listaLlaves, Collections.reverseOrder());
+
+        return listaLlaves;
+    }
+
+    /**
+     * Retorna la cadena que sea lexicográficamente menor dentro de las llaves del mapa .
+     * 
+     * Si el mapa está vacío, debe retornar null.
+     * @return
+     */
+    public String getPrimera( )
+    {
+    	Set<String> set = mapaCadenas.keySet();
+
+        if (set.isEmpty()) {
+            return null;         }
+
+        String primeraCadena = set.stream().min(String::compareTo).orElse(null);
+
+        return primeraCadena;
+    }
+
+    /**
+     * Retorna la cadena que sea lexicográficamente mayor dentro de los valores del mapa
+     * 
+     * Si el conjunto está vacío, debe retornar null.
+     * @return
+     */
+    public String getUltima( )
+    {
+    	Set<String> set = mapaCadenas.keySet();
+
+        if (set.isEmpty()) {
+            return null;         }
+
+        String mayorCadena = set.stream().max(String::compareTo).orElse(null);
+
+        return mayorCadena;
+    }
+
+    /**
+     * Retorna una colección con las llaves del mapa, convertidas a mayúsculas.
+     * 
+     * El orden de las llaves retornadas no importa.
+     * @return Una lista de cadenas donde todas las cadenas están en mayúsculas
+     */
+    public Collection<String> getLlaves( )
+    {
+    	if (mapaCadenas == null) {
+            return null; 
+        }
+
+        
+        return mapaCadenas.keySet().stream().map(String::toUpperCase).toList();
+    }
+
+    /**
+     * Retorna la cantidad de *valores* diferentes en el mapa
+     * @return
+     */
+    public int getCantidadCadenasDiferentes( )
+    {
+    	 if (mapaCadenas == null) {
+             return 0; 
+         }
+
+         Collection<String> valores = mapaCadenas.values();
+         Set<String> valoresUnicos = new HashSet<>(valores);
+
+         return valoresUnicos.size();
+    }
+
+    /**
+     * Agrega un nuevo valor al mapa de cadenas: el valor será el recibido por parámetro, y la llave será la cadena invertida
+     * 
+     * Este método podría o no aumentar el tamaño del mapa, dependiendo de si ya existía la cadena en el mapa
+     * 
+     * @param cadena La cadena que se va a agregar al mapa
+     */
+    public void agregarCadena( String cadena )
+    {
+    	StringBuilder stringBuilder = new StringBuilder(cadena);
+    	mapaCadenas.put(stringBuilder.reverse().toString(), cadena);
+    }
+
+    /**
+     * Elimina una cadena del mapa, dada la llave
+     * @param cadena La llave para identificar el valor que se debe eliminar
+     */
+    public void eliminarCadenaConLLave( String llave )
+    {
+    	if (mapaCadenas != null) {
+            mapaCadenas.remove(llave);
+        }
+    }
+
+    /**
+     * Elimina una cadena del mapa, dado el valor
+     * @param cadena El valor que se debe eliminar
+     */
+    public void eliminarCadenaConValor( String valor )
+    {
+
+    }
+
+    /**
+     * Reinicia el mapa de cadenas con las representaciones como Strings de los objetos contenidos en la lista del parámetro 'objetos'.
+     * 
+     * Use el método toString para convertir los objetos a cadenas.
+     * @param valores Una lista de objetos
+     */
+    public void reiniciarMapaCadenas( List<Object> objetos )
+    {
+
+    }
+
+    /**
+     * Modifica el mapa de cadenas reemplazando las llaves para que ahora todas estén en mayúsculas pero sigan conservando las mismas cadenas asociadas.
+     */
+    public void volverMayusculas( )
+    {
+
+    }
+
+    /**
+     * Verifica si todos los elementos en el arreglo de cadenas del parámetro hacen parte del mapa de cadenas (de los valores)
+     * @param otroArreglo El arreglo de enteros con el que se debe comparar
+     * @return True si todos los elementos del arreglo están dentro de los valores del mapa
+     */
+    public boolean compararValores( String[] otroArreglo )
+    {
+        return false;
+    }
+
+}
